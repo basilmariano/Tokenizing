7d6a210ce3028861cbd1f715dff0387e43aa481b
Panfilo Mariano <panfilomariano@gmail.com>
2017-03-15 14:04:38 +0800

    - initial requirement done

--- TokenizingSample/ViewController.swift
+++ TokenizingSample/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    fileprivate var  addedRecipientColor = UIColor(red: 0, green: 122/255, blue: 1, alpha: 1)
+    fileprivate var  toBeDeletedRecipientColor = UIColor.red.withAlphaComponent(0.8)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let tokenView = KSTokenView(frame: CGRect(x: 0, y: 20, width: UIScreen.main.bounds.width, height: 30))
+        tokenView.delegate = self
+        tokenView.promptText = "    To: "
+        tokenView.placeholder = "Add Recipient"
+        tokenView.descriptionText = "others"
+        tokenView.maxTokenLimit = 1000
+        tokenView.style = .squared
+        tokenView.maximumHeight = 100.0
+        tokenView.paddingX = 10.0
+        tokenView.minimumCharactersToSearch = 3
+        tokenView.showSearchResultController = false
+        //tokenView.minWidthForInput = 120
+        tokenView.layer.shadowColor = UIColor.black.cgColor
+        tokenView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        tokenView.layer.shadowOpacity = 0.5
+        tokenView.layer.shadowRadius = 5
+        tokenView.clipsToBounds = false
+        
+        view.addSubview(tokenView)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +48,20 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: KSTokenViewDelegate {
+    func tokenView(_ token: KSTokenView, performSearchWithString string: String, completion: ((_ results: Array<AnyObject>) -> Void)?) {
+    
+    }
+    
+    func tokenView(_ token: KSTokenView, displayTitleForObject object: AnyObject) -> String {
+        return "displayTitleForObject"
+    }
+    
+    func tokenView(_ tokenView: KSTokenView, shouldChangeAppearanceForToken token: KSToken) -> KSToken? {
+        token.tokenBackgroundHighlightedColor = toBeDeletedRecipientColor
+        token.tokenBackgroundColor = addedRecipientColor
+        token.tokenTextColor = UIColor.white
+        
+        return token
+    }
+}
